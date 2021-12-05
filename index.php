bd9281e437b8d1b3621351284a0e00d1ad0ff8f9
Afnan <72243953+afnan007a@users.noreply.github.com>
2021-12-05 17:36:44 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+/ 1. upload script as "index.php"
+
+// 2. change secret key to something random & unguessable
+
+// 3. ensure the files dir exists and is writable to by the web server
+
+// 4. create a new custom destination on Share-x
+//    > request url: https://yourdomain.com/index.php
+//    > file form name: share_X
+//    > arguments: 
+//      - name: secret_key
+//      - value: your secret key
+
+<?php
+$domainUrl = 'https://yourdomain.com/'; //the you want to use for the image server
+$fileDir = "u/"; //the file directory where the images should be saved or used from
+$fileNameLength = 7; //lenght of the file name
+$secretKey = "secretkey"; //your private secret key
+
+function RandomString($length) {
+    $keys = array_merge(range(0,9), range('a', 'z'));
+    $key = "";
+    
+    for ($i=0; $i < $length; $i++) {
+        $key .= $keys[mt_rand(0, count($keys) - 1)];
+    }
+
+    return $key;
+}
+
+if (isset($_POST['secret_key'])) {
+    if ($_POST['secret_key'] == $secretKey) {
+        $filename = RandomString($fileNameLength);
+        $target_file = $_FILES["share_X"]["name"];
+        $fileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+        if (move_uploaded_file($_FILES["share_X"]["tmp_name"], $fileDir.$filename.'.'.$fileType)) {
+            echo $domainUrl.$fileDir.$filename.'.'.$fileType;
+        } else {
+            echo 'The image failed to upload';
+        }
+    } else {
+        echo 'Secret key is missing or invalid';
+    }
+} else {
+    echo '>No POST data has been receved';
+}
